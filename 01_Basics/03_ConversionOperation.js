c091c33499bfecb41d5666653faf848da06b8099
OmkarPatil4141 <patilomkarr4141@gmail.com>
2024-04-27 20:23:11 +0530

    operations

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -46,6 +46,39 @@ console.log(boolIsLoggedIn)
 
 
 
+/***************************Operations*******************************/
+
+let value = 3;
+
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);   simple working like maths
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+//string operations are somwhow diffrent
+
+let str1 = "Jay";
+let str2 = " Ganesh";
+
+let str3 = str1 + str2; //concat
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+ "2");
+console.log("1"+2+2);
+console.log(2+2+"1");
+
+
+console.log(true);
+console.log(+true);
+
+console.log("");
+console.log(+"");
 
 
 
